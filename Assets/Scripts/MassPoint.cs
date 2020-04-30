35b87f5d053d8b834218a6979ba9cc70faa8cd43
Michael Lohr <michael@lohr-ffb.de>
2020-04-30 14:30:46 +0200

    Update to newer Unity and clean-up

---
+++ Assets/Scripts/MassPoint.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Physics.Integration;
+using UnityEngine;
+
+namespace Assets.Physics
+{
+    public class MassPoint : MonoBehaviour, IPhysicsComponent
+    {
+
+        public Vector3 Position
+        {
+            get { return this.transform.position; }
+            set { this.transform.position = value; }
+        }
+
+        public Vector3 Velocity;
+        public Vector3 Force;   // wihtout spring forces and damping
+        public float Mass = 10f;
+        public float Damping = 0.01f;
+        public bool IsFixed = false;
+
+
+        void Awake()
+        {
+            Simulator.Instance.RegisterMassPoint(this);
+        }
+
+        public void Prepare() // : IPhysicsComponent
+        {
+            AddGravity();   // Note: could also be done directly after clearing forces or before integration
+        }
+
+        public void CleanUp() // : IPhysicsComponent
+        {
+            ClearForces();
+        }
+
+        private void ClearForces()
+        {
+            Force = Vector3.zero;
+        }
+
+        private void AddGravity()
+        {
+            AddAcceleration(Vector3.down * Simulator.Instance.Gravity);
+        }
+
+        // Add a continuous force to the rigidbody, using its mass.
+        public void AddForce(Vector3 force)
+        {
+            Force += force / Mass;
+        }
+
+        //  Add a continuous acceleration to the rigidbody, ignoring its mass.
+        public void AddAcceleration(Vector3 force)
+        {
+            Force += force;
+        }
+
+        public void IntegratePosition(float delta)
+        {
+            Position = Integrator.PerformIntegrationStep(Position, Velocity, delta);
+        }
+
+        public void IntegrateVelocity(float delta, Vector3 computedForce)
+        {
+            if (IsFixed)
+            {
+                // Debug.LogError("Can't add forces to a fixed point.");
+                // just do nothing
+            }
+            else
+            {
+                // Apply force
+                Position = Integrator.PerformIntegrationStep(Position, computedForce, delta);
+            }
+        }
+
+        // Visualize Force Vector
+#if UNITY_EDITOR
+        void OnDrawGizmos()
+        {
+            if (IsFixed)
+                return;
+
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(Position, Position + Force);
+        }
+#endif
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Game-Physics
-My Unity implementation of the game physics lecture exercises (WS17/18) at TUM.
+# Spring System Integration Method Comparison
+
+Following the lecture "Game Physics" at Technical University of Munich (WS17/18), I wanted to implement an actual spring system in unity to compare the different integration methods.
+
+![Screenshot](/images/screenshot.png)
+
+## Requirements
+
+Unity 2019.3
+
+## Usage
+
+Open the project with Unity, double click the scene "Scenes/Demo" and enter "Play"-mode. Now move the cube in the editor or the sphere in the game preview using the arrow/WASD keys.
+
+The integration method can be changed in the Simulator component.
+
+All integration method implementations are contained in the "Scripts/Integration" folder.
+
+## Comparison
+
+To compare the integrators a bit better, I created a [codepen](https://codepen.io/michidk/pen/VymawG) to visualize the performance on a simple function:
+
+![Comparison](/images/comparison.png)
+
+As you can see, Runge-Kutta integration (RK4) ist the most accurate method. This was to be expected since its a fourth-order method, which is the highest order of all tested methods.
